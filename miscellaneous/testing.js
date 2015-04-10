dafc532591d59bf96d443d0e92f15be2da0149b9
Nikhil Joy <nikhil.joy@seeq.com>
2015-04-10 14:25:16 -0400

    removed unnecessary files

--- miscellaneous/testing.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var Deserializer = require('./seequmber/deserializer');
-var directory = __dirname + '/seequmber/features/';
-var deserializer = new Deserializer(directory);
-deserializer.deserialize();
