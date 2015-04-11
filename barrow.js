08d852ea8bfdda8bc6bc87cda1cb60eec2169660
Nikhil Joy <nikhil.joy@seeq.com>
2015-04-10 22:09:06 -0400

    added ejs capability to barrow

--- barrow.js
+++ barrow.js
@@ -1,9 +1,23 @@
 'use strict';
 
 var fs = require('fs');
+var ejs = require('ejs');
 var barrow = {
     deserializer: require('./seequmber/deserializer'),
     serializer: require('./seequmber/serializer'),
-    getEJS: fs.readFileSync(__dirname + '/seequmber/serializer.ejs').toString()
+    renderHTML: function renderHTML(directory) {
+        var ejsString = fs.readFileSync(__dirname + '/seequmber/serializer.ejs').toString();
+        var deserializer = new barrow.deserializer(directory);
+        var features = deserializer.deserialize();
+        var html = '';
+        if (features) {
+            html = ejs.render(ejsString, {
+                data: features
+            });
+            html = html.replace(/&#34;/g, '"');
+            html = html.replace(/^\s*[\r\n]/gm, '');
+        }
+        return html;
+    }
 };
 module.exports = barrow;
