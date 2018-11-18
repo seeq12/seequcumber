c0c76c5f5568a1851fc63039fd738629e24feb2d
Mark Derbecker <mark.derbecker@seeq.com>
2018-11-17 16:22:28 -0800

    Enable strict type checking and add clime as command line parser

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,17 @@ export * from './lib/async';
 export * from './lib/hash';
 export * from './lib/number';
 
-console.log("Hello Elise!");
+import * as Path from 'path';
+import { CLI, Shim } from 'clime';
+
+// The second parameter is the path to folder that contains command modules.
+let cli = new CLI('seequcumber', Path.join(__dirname, 'commands'));
+
+// Clime in its core provides an object-based command-line infrastructure.
+// To have it work as a common CLI, a shim needs to be applied:
+let shim = new Shim(cli);
+shim.execute(process.argv).then(() => {
+  // Do nothing when all is said and done. (Gets rid of IntelliJ warning.)
+});
+
+
